676cdf0442add83a63557097a8ab3afce52fe9d0
dhha <doanhuuha@gmail.com>
2023-04-04 14:22:12 -0500

    Finish

--- app/src/main/java/com/example/assignments_2/MainActivity.kt
+++ app/src/main/java/com/example/assignments_2/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.assignments_2
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.assignments_2.databinding.ActivityMainBinding
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            var rnd: Random = Random()
+            var color = Color.argb(rnd.nextInt(255), rnd.nextInt(255), rnd.nextInt(255), rnd.nextInt(255))
+            binding.mainActivity.setBackgroundColor(color)
+        }
+
+        binding.btnImage.setOnClickListener {
+            var rnd: Random = Random()
+            var color = Color.argb(rnd.nextInt(255), rnd.nextInt(255), rnd.nextInt(255), rnd.nextInt(255))
+            it.setBackgroundColor(color)
+        }
     }
 }
